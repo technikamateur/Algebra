89fa8ae5e9e09cb6e9e4f289e54f75766eacf27f
Daniel <technikamateur@gmx.de>
2018-06-10 17:13:23 +0200

    so ein paar Skripte

---
+++ add_zyklische_gruppen.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+def checker(zuPrüfen, dieZahl):
+	x = -1
+	count = 1
+	while (x != zuPrüfen):
+		count = count + 1
+		if (count == dieZahl):
+			break
+		if (x == -1):
+			x = zuPrüfen
+		x = x + zuPrüfen
+		if (x > dieZahl):
+			x = x % dieZahl
+	if (x == zuPrüfen or x == 0):
+		return(False)
+	else:
+		return(True)
+		
+
+
+zahlen = []
+erg = []
+zahl = int(input("Max. Zahl eingeben: "))
+alleZahlen = input("Auch Zhalen von 0 bis deine Zahl (j für ja)? ")
+if (alleZahlen == "j"):
+	for i in range(1, zahl):
+		zahlen.append(i)
+else:
+	zahlen.append((zahl - 1))
+
+for element in zahlen:
+	if checker(element, zahl):
+		erg.append(element)
+
+print(erg)
